a2fc32616fa859a1265693e69efe2394ba839008
prestonh7 <prestonhouse7@gmail.com>
2023-04-14 14:09:06 -0500

    Basic styles, color

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import './styles.css'
+import "./styles.css";
+import "/images/menu.svg";
+import "/images/home.svg";
+import "/images/inbox.svg";
+import "/images/today.svg";
+import "/images/calendar_month.svg";
 
-console.log('Working!');
+const sideBar = (() => {
+  const headerHeight = document.querySelector("header").offsetHeight;
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.top = headerHeight + "px";
+})();
