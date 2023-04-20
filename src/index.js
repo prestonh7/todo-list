ab7d907b0c98b8848045779e4f3ffc3b63ea59c8
prestonh7 <prestonhouse7@gmail.com>
2023-04-19 19:56:01 -0500

    Another image, add task makes a popup

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ import "/images/home.svg";
 import "/images/inbox.svg";
 import "/images/today.svg";
 import "/images/calendar_month.svg";
+import "/images/add.svg"
 
 const sideBar = (() => {
     const header = document.querySelector('header');
     const headerHeight = getComputedStyle(header).getPropertyValue('height');
     document.documentElement.style.setProperty('--header-height', headerHeight);
+})();
+
+const addTask = (() => {
+    const addTaskBtn = document.querySelector('#addTaskBtn');
+    const popup = document.querySelector('#popup');
+
+    addTaskBtn.addEventListener('click', () => {
+        popup.classList.toggle('open');
+    })
 })();
