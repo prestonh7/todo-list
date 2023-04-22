103bb77ac0948a1a9a849660aa34e696251a765e
prestonh7 <prestonhouse7@gmail.com>
2023-04-22 16:39:38 -0500

    Add new tasks works

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ const displayController = (() => {
     const container = document.querySelector('.content');
 
     function drawListToScreen() {
+        container.innerText = '';
         taskList.tasks.forEach((todo, index) => {
             const div = document.createElement('div');
             const taskName = document.createElement('h4');
@@ -60,17 +61,22 @@ const addTaskPopup = (() => {
         popup.classList.toggle('open');
     });
 
-    form.addEventListener('submit', () => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
         const task = form.querySelector('#task').value;
         const deadline = form.querySelector('#deadline').value;
         const todo = new Task(task, deadline);
+        taskList.addTask(todo);
+        form.reset();
+        popup.classList.toggle('open');
+        display.drawListToScreen();
     });
 
     cancelBtn.addEventListener('click', () => {
         form.reset();
         popup.classList.toggle('open');
     });
-})();
+});
 
 class Task {
     constructor(task, deadline, completed = false) {
@@ -78,20 +84,13 @@ class Task {
         this.deadline = deadline;
         this.completed;
     }
-
-    addToList(list) {
-        list.push(this);
-    }
-
-    removeFromList(list) {
-        list.splice(this);
-    }
 }
 
 const taskList = taskListHandler();
 const display = displayController();
+const popup = addTaskPopup();
 
-const task1 = new Task('Task 1', '12/24/1997');
+const task1 = new Task('Task 1', '12-24-1997');
 
 taskList.addTask(task1);
 
